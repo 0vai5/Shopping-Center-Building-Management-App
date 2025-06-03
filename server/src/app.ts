4572f907f4b779a4bdfe8fb76963e116bde26f47
0vai5 <rovais53@gmail.com>
2025-06-03 19:49:15 +0500

    feat: refactor server structure and integrate serverless-http for deployment

--- server/src/app.ts
+++ server/src/app.ts
@@ -38,4 +38,6 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
+export default app;
