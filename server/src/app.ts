0375ce11598906191c8b10c716d32c0c7c3364df
0vai5 <rovais53@gmail.com>
2025-06-03 20:07:07 +0500

    refactor: update Vercel configuration and main entry point for server

--- server/src/app.ts
+++ server/src/app.ts
@@ -38,6 +38,5 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello, World!");
 });
 
-// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
-export default app;
